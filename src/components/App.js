418b2f887f40e0e30807b2f5ac8192ade2999ab9
Samira Gadri <samira.gadri@loginradius.com>
2020-09-15 08:41:37 -0700

    implemented add nom, remove nom, banner

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,129 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Row, Col, Button, Alert } from "react-bootstrap";
+import styled from "styled-components";
 
-import SearchBar from './SearchBar'
+import SearchBar from "./SearchBar";
 
-const App = ()  => {
+const App = () => {
+  const [movieResults, setMoviesResults] = useState([]);
+  const [nominations, setNominations] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("rambo");
+  const [loading, setLoading] = useState(false);
+  const [totalResults, setTotalResults] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const [movieResults, setMoviesResults] = useState([])
-  const [searchTerm, setSearchTerm] = useState('rambo')
-  const [loading, setLoading] = useState(false)
-  const [totalResults, setTotalResults] = useState(0)
-  const [currentPage, setCurrentPage] = useState(1)
-  const [moviesPerPage, setMoviesPerPage] = useState(20) 
-  
-  
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      console.log("handle submit called")
-      setLoading(true)
-      debugger
-      const res = await axios.get(`http://www.omdbapi.com/?apikey=${[process.env.REACT_APP_APIKEY]}&s=${searchTerm}&type=movie&page=${currentPage}`)
-      setMoviesResults(res.data.Search)
-      setTotalResults(res.data.totalResults)
+      setLoading(true);
+      const res = await axios.get(
+        `http://www.omdbapi.com/?apikey=${[
+          process.env.REACT_APP_APIKEY,
+        ]}&s=${searchTerm}&type=movie&page=${currentPage}`
+      );
+      setMoviesResults(res.data.Search);
+      setTotalResults(res.data.totalResults);
     } catch (e) {
-      console.warn("something wrong happened", e)
+      console.warn("something wrong happened", e);
     } finally {
-      setLoading(false)
-    }    
-  }
+      setLoading(false);
+    }
+  };
 
   const handleChange = (e) => {
-    setSearchTerm(e.target.value)
-  }
+    setSearchTerm(e.target.value);
+  };
+
+  const handleNomination = (title, year) => {
+    setNominations([...nominations, { Title: title, Year: year }]);
+  };
+
+  const handleRemoveNomination = (title, year) => {
+    setNominations(
+      nominations.filter((movie) => {
+        return movie.Title !== [title] && movie.Year !== year;
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <h1>The Shoppies</h1>
-      <SearchBar handleSubmit={handleSubmit} handleChange={handleChange} />
+      <Container>
+        <Row>
+          <h1>The Shoppies</h1>
+        </Row>
+        <Row>
+          <SearchBar handleSubmit={handleSubmit} handleChange={handleChange} />
+        </Row>
+        <Row>
+          <Col>
+            {nominations.length == 5 ? (
+              <Banner variant="success">You have selected 5 nominations</Banner>
+            ) : (
+              <Banner variant="light">{` `}</Banner>
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {`Results for "${searchTerm}"`}
+            <ul>
+              {movieResults.map((movie, index) => {
+                const title = movie.Title;
+                const year = movie.Year;
+                let btnDisabled = nominations.length === 5 ? true : false;
+
+                if (
+                  nominations.some(
+                    (movie) => movie.Title === title && movie.Year === year
+                  )
+                ) {
+                  btnDisabled = true;
+                }
+                return (
+                  <li key={index}>
+                    {`${title} (${year}) `}
+                    <Button
+                      onClick={() => handleNomination(title, year)}
+                      disabled={btnDisabled}
+                    >
+                      Nominate
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          </Col>
+
+          <Col>
+            Nominations
+            <ul>
+              {nominations.map((movie, index) => {
+                const title = movie.Title;
+                const year = movie.Year;
+                return (
+                  <li key={index}>
+                    {`${title} (${year}) `}
+                    <Button
+                      onClick={() => handleRemoveNomination(title, year)}
+                      disabled={false}
+                    >
+                      Remove
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
+
+const Banner = styled(Alert)`
+  width: 100%;
+`;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Container } from 'reactstrap'
+import { Container, Row, Col  } from 'react-bootstrap';
 
 const SearchBar = (props) => {
     return (
         <Container>
-            <div className="row">
-                <section className="">
+            <Row>
+                <Col>
                     <form action="" onSubmit={props.handleSubmit}>
-                    <div className="input-field">
                     <label> Movie Title</label>
-                    <input placeholder="Search" type="text" onChange={props.handleChange}></input>
-                    </div>
+                    <input placeholder="Search" type="text" onChange={props.handleChange} size={100}></input>
                     </form>
-                </section>
-            </div>
+                    </Col>
+            </Row>
         </Container>
     )
 }
